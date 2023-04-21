742e42e4dc44f43ccc5f869fde04dc87e8adb3b4
John Xia <xiajohn@hotmail.com>
2023-04-20 20:08:20 -0700

    add default essay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,12 +26,19 @@ function App() {
 function HomePage() {
   const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
-    Topic: '',
+    Topic: 'Pixel Pen',
     Grade: '12',
     Word_Count: '100',
     Language: 'English',
   });
-  const [essay, setEssay] = useState('');
+  const [essay, setEssay] = useState(
+    "Title: The Legend of Pixel Pen\n\n" +
+    "In a world of boundless creativity, there was a village called Pixellia, famous for its inhabitants' storytelling prowess. A young inventor named Penelope lived there, and she had created a device called 'Pixel Pen,' which could transform ideas into tangible forms.\n\n" +
+    "Fast forward to modern society, advances in artificial intelligence have made it possible to bring the legend of Pixel Pen to life. This AI-driven platform harnesses the power of Penelope's mythical invention to help people transform their ideas into compelling narratives, engaging speeches, and captivating visuals.\n\n" +
+    "In the digital age, where ideas travel at the speed of light, Pixel Pen has become an indispensable tool for creators and innovators. As it streamlines the creative process, it empowers individuals to focus on the essence of their ideas, while the AI handles the intricacies of expression.\n\n" +
+    "The legend of Pixel Pen lives on, inspiring countless creators to tap into their imaginative potential, and in doing so, shape the future of human ingenuity.\n\n" +
+    "So come, write down your idea with Pixel Pen and witness the magic of your vision coming to life!"
+  );
 
   return (
     <>
--- frontend/src/components/FormComponent.js
+++ frontend/src/components/FormComponent.js
@@ -2,44 +2,103 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 import '../css/Form.css';
-import {getServerURL} from '../util/utils';
+import { getServerURL } from '../util/utils';
 
 function FormComponent({ formData, setFormData, setEssay, setIsLoading }) {
   const [showTopicError, setShowTopicError] = useState(false);
+  const [showWordCountError, setShowWordCountError] = useState(false);
+  const [showTopicChangedError, setShowTopicChangedError] = useState(false);
 
+  const initialTopic = 'Pixel Pen'; // Set the initial topic value here
+
+  // ... (rest of the code)
+
+  // Add error messages for word count and topic change in the form
+  {showWordCountError && (
+    <div className="form-error" style={{ color: 'red' }}>
+      Word count must be less than 2000
+    </div>
+  )}
+  
+  {showTopicChangedError && (
+    <div className="form-error" style={{ color: 'red' }}>
+      Please change the topic
+    </div>
+  )}
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
     setShowTopicError(false);
   };
 
+  function validateFormData(formData) {
+    // Initialize validation flags
+    let valid = true;
+  
+    // Check if the topic field is empty
+    if (!formData.Topic) {
+      setShowTopicError(true);
+      valid = false;
+    } else {
+      setShowTopicError(false);
+    }
+  
+    // Check if the word count is less than 2000
+    if (formData.WordCount > 2000) {
+      setShowWordCountError(true);
+      valid = false;
+    } else {
+      setShowWordCountError(false);
+    }
+  
+    // Check if the topic has changed
+    if (formData.Topic === initialTopic) {
+      setShowTopicChangedError(true);
+      valid = false;
+    } else {
+      setShowTopicChangedError(false);
+    }
+  
+    return valid;
+  }
+  
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsLoading(true);
     let generateEssayURL = getServerURL() + '/generate-essay';
-
-    // Check if the topic field is empty
-    if (!formData.Topic) {
-      setShowTopicError(true);
+  
+    // Check if the topic has changed
+    if (formData.Topic === initialTopic) {
+      setShowTopicChangedError(true);
+      setIsLoading(false);
+      return;
+    } else {
+      setShowTopicChangedError(false);
+    }
+  
+    if (validateFormData(formData)) {
+      const payload = {
+        grade: formData.Grade,
+        wordCount: formData.Word_Count,
+        topic: formData.Topic,
+        language: formData.Language,
+      };
+  
+      try {
+        const response = await axios.post(generateEssayURL, payload);
+        const essay = response.data.essay;
+        console.log('Generated Essay:', essay);
+        setEssay(essay);
+      } catch (error) {
+        console.error('Error generating essay:', error.message);
+      }
+      setIsLoading(false);
+    } else {
       setIsLoading(false);
       return;
     }
-     const payload = {
-      grade: formData.Grade,
-      wordCount: formData.Word_Count,
-      topic: formData.Topic,
-      language: formData.Language,
-    };
-
-    try {
-      const response = await axios.post(generateEssayURL, payload);
-      const essay = response.data.essay;
-      console.log('Generated Essay:', essay);
-      setEssay(essay);
-    } catch (error) {
-      console.error('Error generating essay:', error.message);
-    }
-    setIsLoading(false);
   };
+  
 
   return (
     <Form onSubmit={handleSubmit} className="form">
