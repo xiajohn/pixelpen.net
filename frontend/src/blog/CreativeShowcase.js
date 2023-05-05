982798748311eae6e789ff24092866018c202543
John Xia <xiajohn@hotmail.com>
2023-05-04 22:32:25 -0700

    ui update

--- frontend/src/blog/CreativeShowcase.js
+++ frontend/src/blog/CreativeShowcase.js
@@ -1,6 +1,10 @@
+// CreativeShowcase.js
+
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { fetchMetadata } from '../util/utils.js'
+import { fetchMetadata } from '../util/utils.js';
+import '../css/CreativeShowcase.css'; // Import the CSS file
+
 const CreativeShowcase = () => {
   const [blogs, setBlogs] = useState({});
 
@@ -11,22 +15,23 @@ const CreativeShowcase = () => {
         setBlogs(blogData);
       }
     };
-  
+
     fetchBlogs();
   }, []);
-
   return (
-    <div style={{ padding: '20px' }}>
+    <div className="CreativeShowcase">
       <h1>Pixel Pen Creative Showcase</h1>
       <p>
-        Welcome to the Pixel Pen Creative Showcase, where we promote our partners and their innovative ideas. Feel free to explore the featured products and stories from our current clients. These blogs serve as a powerful platform for generating attention and showcasing your idea to the world.
+        Welcome to the Pixel Pen Creative Showcase, where we promote our
+        partners and their innovative ideas. Feel free to explore the featured
+        products and stories from our current clients. These blogs serve as a
+        powerful platform for generating attention and showcasing your idea to
+        the world.
       </p>
       {Object.keys(blogs).map((key) => (
-        <div key={key}>
-          <Link to={`/blog/${key}`}>
-            <h2>{blogs[key].title}</h2>
-          </Link>
-        </div>
+        <Link key={key} to={`/blog/${key}`} className="blog-link">
+          <h2>{blogs[key].title}</h2>
+        </Link>
       ))}
     </div>
   );
