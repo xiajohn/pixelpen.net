a0fe89dcadc2bff300b86682b91e9a582959436c
John Xia <xiajohn@hotmail.com>
2023-05-13 23:06:49 -0700

    makememe

--- infra/run.py
+++ infra/run.py
@@ -1,5 +1,6 @@
 from blogPipeline.main import writeBlogs
 from recurringTasks.email.main import sendEmails
 from recurringTasks.social_media.facebookCreator import createFacebookPost
+from recurringTasks.makememe.make import make
 if __name__ == "__main__":
-    sendEmails()
+    make("lost something")
