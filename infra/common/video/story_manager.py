e36217f6d90c91849e5e1d3861434fe08ec4ca5e
John Xia <xiajohn@hotmail.com>
2023-06-07 19:19:54 -0700

    story

--- infra/common/video/story_manager.py
+++ infra/common/video/story_manager.py
@@ -1,14 +1,31 @@
 from moviepy.editor import concatenate_videoclips, VideoFileClip, AudioFileClip, ImageClip, CompositeVideoClip, CompositeAudioClip
 from common.metadata_manager import MetadataManager
-from moviepy.video.fx.all import fadein
+from moviepy.video.fx.all import fadein, fadeout
 import os
 from common.constants import Constants
+
 class StoryManager:
     def __init__(self, folder_name):
         self.folder_name = folder_name
         self.metadata_manager = MetadataManager()
 
-    
+    def getSlideInAndFadeOutImageClip(self, image_file, start_time, duration, video_size):
+        """Returns an ImageClip with slide-in and fade-in effects applied."""
+        fade_duration = 2
+        half_duration = (duration - fade_duration) / 2
+        image = (ImageClip(image_file)
+                 .resize(height=int(video_size[1] * 0.8))  # Resize image to 80% of the height of the video before adding it to the clip
+                 .set_duration(duration)  # Slide-in phase lasts for half of the duration
+                 .set_start(start_time)
+                 .fadein(1)  # Apply the fade-in effect over 1 second
+                 .fadeout(fade_duration)  # Apply the fade-out effect over 2 seconds
+                 .set_position(lambda t: (min(t/half_duration * video_size[0]/2, video_size[0]/2), 'center'))  # Slide in then stop
+                 )
+        return image
+
+    def getSortedImageFiles(self, images_folder):
+        """Returns a sorted list of image file paths from the given folder."""
+        return sorted([os.path.join(images_folder, f) for f in os.listdir(images_folder) if f.endswith(('.png', '.jpg'))])
 
     def addImageToVideo(self, video_path, images_folder):
         original = video_path[0:-4]
@@ -18,8 +35,7 @@ class StoryManager:
             return final_location
         video = VideoFileClip(video_path)
 
-        # Get a sorted list of all image file paths in the directory
-        image_files = sorted([os.path.join(images_folder, f) for f in os.listdir(images_folder) if f.endswith(('.png'))])
+        image_files = self.getSortedImageFiles(images_folder)
 
         clips = [video]  # Start with the video as the first clip
 
@@ -29,15 +45,11 @@ class StoryManager:
             if start_time + duration > video.duration - 2:  # Stop adding images 2 seconds before the video ends
                 break
 
-            image = (ImageClip(image_file)
-                    .set_duration(duration)
-                    .set_start(start_time)
-                  #  .resize(620)
-                    .set_position(('center', 'center')))
-            clips.append(image)
+            image_clip = self.getSlideInAndFadeOutImageClip(image_file, start_time, duration, video.size)
+            clips.append(image_clip)
 
         final_clip = CompositeVideoClip(clips)
 
         final_clip.write_videofile(f'{final_location}', codec='libx264')
 
-        return f'{final_location}'
+        return final_location
