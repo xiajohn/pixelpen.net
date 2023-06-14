8fbb52c0441850614bed31d14d623a60bf475c56
John Xia <xiajohn@hotmail.com>
2023-06-13 18:54:48 -0700

    blog post

--- infra/run.py
+++ infra/run.py
@@ -5,6 +5,6 @@ from common.makememe.make import make
 from common.video.video_generator import makeVideo
 from clients.midjourney_api import MidjourneyApi
 if __name__ == "__main__":
-    makeVideo()
+    writeBlogs()
    # MidjourneyApi("product advertising", "video/how-to-market-your-product/images/image2.png")
 
